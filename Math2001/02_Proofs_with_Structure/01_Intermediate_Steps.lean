620648ae09513d5b7aff83e293d81a397b81358f
Saint Wesonga <saintwesonga@gmail.com>
2025-09-02 22:22:11 -0600

    Add more section 2.1 solutions

--- Math2001/02_Proofs_with_Structure/01_Intermediate_Steps.lean
+++ Math2001/02_Proofs_with_Structure/01_Intermediate_Steps.lean
@@ -92,16 +92,39 @@ example (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
     _ = b^2 := by ring
 
 example (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by
-  sorry
+  have h3 : 0 ≤ b - a := by addarith [h]
+  calc
+    a ^ 3 ≤ a ^ 3 + ((b - a) * ((b - a)^2 + 3 * (b + a)^2))/4 := by extra
+        _ = b ^ 3 := by ring
 
 /-! # Exercises -/
 
 
 example {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by
-  sorry
+  have h3: x * (x + 2) = 2 * (x + 2) :=
+  calc
+    x * (x + 2) = x^2 + 2 * x := by ring
+    _ = 4 + 2 * x := by rw [h1]
+    _ = 2 * x + 4 := by ring
+    _ = 2 * (x + 2) := by ring
+  cancel x+2 at h3
 
+/-
+Accidentally stumbled into the solution. I was trying to make cancellation of
+2 a hypothesis called h3 and uncommented the calc block following the
+"cancel 2 at h2" line.
+-/
 example {n : ℤ} (hn : n ^ 2 + 4 = 4 * n) : n = 2 := by
-  sorry
+  have h2: (n - 2)^2 = 0 :=
+  calc
+    (n - 2)^2 = n^2 - 4*n + 4 := by ring
+    _ = (n^2 + 4) - 4 * n := by ring
+    _ = 4 * n - 4 * n := by rw [hn]
+    _ = 0 := by ring
+  --have h3: n - 2 = 0 :=
+  cancel 2 at h2
+  calc
+    n = 2 := by addarith [h2]
 
 example (x y : ℚ) (h : x * y = 1) (h2 : x ≥ 1) : y ≤ 1 := by
   sorry
