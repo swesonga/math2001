7c73025f1f32d8a2152628a7b2735c6c4365a2f2
Saint Wesonga <saintwesonga@gmail.com>
2025-09-14 00:47:35 -0600

    Add solutions to some examples from 2.5

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -34,13 +34,22 @@ example (x : ℝ) : ∃ y : ℝ, y > x := by
 
 
 example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 := by
-  sorry
+  use 6,5
+  ring
 
 example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 := by
-  sorry
+  use a+1, a
+  ring
 
 example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q := by
-  sorry
+  use (p + q)/2
+  constructor
+  calc
+    p = (p + p)/2 := by ring
+    _ < (p + q)/2 := by rel [h]
+  calc
+    (p + q)/2 < (q + q)/2 := by rel [h]
+    _ = q := by ring
 
 example : ∃ a b c d : ℕ,
     a ^ 3 + b ^ 3 = 1729 ∧ c ^ 3 + d ^ 3 = 1729 ∧ a ≠ c ∧ a ≠ d := by
