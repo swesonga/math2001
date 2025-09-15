961ab5c82f0d86aeba1e1d1e218185067bf486a7
Saint Wesonga <saintwesonga@gmail.com>
2025-09-14 22:01:49 -0600

    Solve exercises 1-12 of 3.1.10

--- Math2001/03_Parity_and_Divisibility/01_Parity.lean
+++ Math2001/03_Parity_and_Divisibility/01_Parity.lean
@@ -101,40 +101,130 @@ example (n : ℤ) : Even (n ^ 2 + n + 4) := by
 
 
 example : Odd (-9 : ℤ) := by
-  sorry
+  use -5
+  numbers
 
 example : Even (26 : ℤ) := by
-  sorry
+  use 13
+  numbers
 
 example {m n : ℤ} (hm : Odd m) (hn : Even n) : Odd (n + m) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨m1, hm'⟩ := hm
+  obtain ⟨n1, hn'⟩ := hn
+  have ht := calc
+    n + m = 2 * n1 + (2 * m1 + 1) := by rw [hm', hn']
+    _ = 2 * (n1 + m1) + 1 := by ring
+  use n1 + m1
+  apply ht
 
 example {p q : ℤ} (hp : Odd p) (hq : Even q) : Odd (p - q - 4) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨p1, hp'⟩ := hp
+  obtain ⟨q1, hq'⟩ := hq
+  have ht := calc
+    p - q - 4 = 2 * p1 + 1 - 2 * q1 - 4 := by rw [hp', hq']
+    _ = 2 * (p1 - q1 - 2) + 1 := by ring
+  use p1 - q1 - 2
+  apply ht
 
 example {a b : ℤ} (ha : Even a) (hb : Odd b) : Even (3 * a + b - 3) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨a1, ha'⟩ := ha
+  obtain ⟨b1, hb'⟩ := hb
+  have ht := calc
+    3 * a + b - 3 = 3 * (2 * a1) + (2 * b1 + 1) - 3 := by rw [ha', hb']
+    _ = 2 * (3 * a1) + (2 * b1 + 1) - 3 := by ring
+    _ = 2 * (3 * a1 + b1 - 1) := by ring
+  use 3 * a1 + b1 - 1
+  apply ht
 
 example {r s : ℤ} (hr : Odd r) (hs : Odd s) : Even (3 * r - 5 * s) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨r1, hr'⟩ := hr
+  obtain ⟨s1, hs'⟩ := hs
+  have ht := calc
+    3 * r - 5 * s = 3 * (2 * r1 + 1) - 5 * (2 * s1 + 1) := by rw [hr', hs']
+    _ = 2 * 3 * r1 + 3 - 2 * 5 * s1 - 5 := by ring
+    _ = 2 * (3 * r1 - 5 * s1 - 1) := by ring
+  use 3 * r1 - 5 * s1 - 1
+  apply ht
 
 example {x : ℤ} (hx : Odd x) : Odd (x ^ 3) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨m, hx'⟩ := hx
+  have ht := calc
+    x^3 = (2 * m + 1)^3 := by rw [hx']
+    _ = (2 * m + 1) * (2 * m + 1)^2 := by ring
+    _ = (2 * m + 1) * (4 * m^2 + 4 * m + 1) := by ring
+    _ = (2 * m + 1) * (4 * m^2 + 4 * m + 1) := by ring
+    _ = (8 * m^3 + 8 * m^2 + 2*m) + (4 * m^2 + 4 * m + 1) := by ring
+    _ = 8 * m^3 + 12 * m^2 + 6 * m + 1 := by ring
+    _ = 2 * (4 * m^3 + 6 * m^2 + 3 * m) + 1 := by ring
+  use (4 * m^3 + 6 * m^2 + 3 * m)
+  apply ht
 
 example {n : ℤ} (hn : Odd n) : Even (n ^ 2 - 3 * n + 2) := by
-  sorry
+  dsimp [Odd, Even] at *
+  obtain ⟨m, hn'⟩ := hn
+  have ht := calc
+    n ^ 2 - 3 * n + 2 = (2 * m + 1) ^ 2 - 3 * (2 * m + 1) + 2 := by rw [hn']
+    _ = (4 * m^2 + 4 * m + 1) - 3 * (2 * m + 1) + 2 := by ring
+    _ = (4 * m^2 - 2 * m) := by ring
+    _ = 2 * (2 * m^2 - m) := by ring
+  use 2 * m^2 - m
+  apply ht
 
 example {a : ℤ} (ha : Odd a) : Odd (a ^ 2 + 2 * a - 4) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨m, ha'⟩ := ha
+  have ht := calc
+    a ^ 2 + 2 * a - 4 = (2 * m + 1) ^ 2 + 2 * (2 * m + 1) - 4 := by rw [ha']
+    _ = (4 * m^2 + 4 * m + 1) + 2 * (2 * m + 1) - 4 := by ring
+    _ = (4 * m^2 + 8 * m - 1) := by ring
+    _ = 2 * (2 * m^2 + 4 * m - 1) + 1 := by ring
+  use 2 * m^2 + 4 * m - 1
+  apply ht
 
 example {p : ℤ} (hp : Odd p) : Odd (p ^ 2 + 3 * p - 5) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨m, hp'⟩ := hp
+  have ht := calc
+    p ^ 2 + 3 * p - 5 = (2 * m + 1) ^ 2 + 3 * (2 * m + 1) - 5 := by rw [hp']
+    _ = (4 * m^2 + 4 * m + 1) + 3 * (2 * m + 1) - 5 := by ring
+    _ = (4 * m^2 + 10 * m - 1) := by ring
+    _ = 2 * (2 * m^2 + 5 * m - 1) + 1 := by ring
+  use 2 * m^2 + 5 * m - 1
+  apply ht
 
 example {x y : ℤ} (hx : Odd x) (hy : Odd y) : Odd (x * y) := by
-  sorry
+  dsimp [Odd] at *
+  obtain ⟨k1, hx'⟩ := hx
+  obtain ⟨k2, hy'⟩ := hy
+  have ht := calc
+    x * y = (2 * k1 + 1) * (2 * k2 + 1) := by rw [hx', hy']
+    _ = 4 * k1 * k2 + 2 * k1 + 2 * k2 + 1 := by ring
+    _ = 2 * (2 * k1 * k2 + k1 + k2) + 1 := by ring
+  use 2 * k1 * k2 + k1 + k2
+  apply ht
 
 example (n : ℤ) : Odd (3 * n ^ 2 + 3 * n - 1) := by
-  sorry
+  obtain hn | hn := Int.even_or_odd n -- switch on whether n is odd or even
+  dsimp [Odd, Even] at *
+  · obtain ⟨k1, hn'⟩ := hn
+    have ht := calc
+      3 * n ^ 2 + 3 * n - 1 = 3 * (2 * k1) ^ 2 + 3 * (2 * k1) - 1 := by rw [hn']
+      _ = 2 * 6 * k1 ^ 2 + 3 * (2 * k1) - 1 := by ring
+      _ = 2 * (6 * k1 ^ 2 + 3 * k1 - 1) + 1 := by ring
+    use 6 * k1 ^ 2 + 3 * k1 - 1
+    apply ht
+  · obtain ⟨k1, hn'⟩ := hn
+    have ht := calc
+      3 * n ^ 2 + 3 * n - 1 = 3 * (2 * k1 + 1) ^ 2 + 3 * (2 * k1 + 1) - 1 := by rw [hn']
+      _ = 3 * (4 * k1 ^ 2 + 4 * k1 + 1) + 6 * k1 + 3 - 1 := by ring
+      _ = 2 * (6 * k1 ^ 2 + 9 * k1 + 2) + 1 := by ring
+    use 6 * k1 ^ 2 + 9 * k1 + 2
+    apply ht
 
 example (n : ℤ) : ∃ m ≥ n, Odd m := by
   sorry
