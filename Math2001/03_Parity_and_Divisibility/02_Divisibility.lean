b4a786daddc45ea6d08d00acb9ecdd42cb6537a6
Saint Wesonga <saintwesonga@gmail.com>
2025-09-15 21:42:05 -0600

    Complete examples from section 3.2

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -12,7 +12,9 @@ example : (11 : ℕ) ∣ 88 := by
 
 
 example : (-2 : ℤ) ∣ 6 := by
-  sorry
+  dsimp [(· ∣ ·)]
+  use -3
+  numbers
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
   obtain ⟨k, hk⟩ := hab
@@ -23,10 +25,23 @@ example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
 
 
 example {a b c : ℕ} (hab : a ∣ b) (hbc : b ^ 2 ∣ c) : a ^ 2 ∣ c := by
-  sorry
+  obtain ⟨k1, hk1⟩ := hab
+  obtain ⟨k2, hk2⟩ := hbc
+  have ht := calc
+    c = b^2 * k2 := hk2
+    _ = (a * k1) ^ 2 * k2 := by rw [hk1]
+    _ = a^2 * (k1 ^ 2 * k2) := by ring
+  dsimp [(· ∣ ·)]
+  use k1 ^ 2 * k2
+  apply ht
 
 example {x y z : ℕ} (h : x * y ∣ z) : x ∣ z := by
-  sorry
+  obtain ⟨k1, hk1⟩ := h
+  dsimp [(· ∣ ·)]
+  use y * k1
+  calc
+    z = x * y * k1 := hk1
+    _ = x * (y * k1) := by ring
 
 example : ¬(5 : ℤ) ∣ 12 := by
   apply Int.not_dvd_of_exists_lt_and_lt
@@ -51,7 +66,11 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
 
 
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
-  sorry
+  obtain ⟨k, hk⟩ := hab
+  have ht := calc
+    0 < b := hb
+    _ = a * k := hk
+  cancel k at ht
 
 /-! # Exercises -/
 
