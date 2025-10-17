2fbc6cf73a2f3979e85e8c45e63d853893878a66
Saint Wesonga <saintwesonga@gmail.com>
2025-10-17 00:42:56 -0400

    Complete section 5.1

--- Math2001/05_Logic/01_Logical_Equivalence.lean
+++ Math2001/05_Logic/01_Logical_Equivalence.lean
@@ -43,7 +43,17 @@ example (P Q R : Prop) : (P ∧ (Q ∨ R)) ↔ ((P ∧ Q) ∨ (P ∧ R)) := by
       constructor
       · apply h1
       · apply h2
-  · sorry
+  · intro h
+    obtain ha | hb := h -- <;>
+    constructor <;> obtain ⟨h3, h4⟩ := ha
+    · apply h3
+    · left
+      apply h4
+    constructor <;> obtain ⟨h3, h4⟩ := hb
+    · apply h3
+    right
+    apply h4
+
 
 #truth_table P ∧ (Q ∨ R)
 #truth_table (P ∧ Q) ∨ (P ∧ R)
@@ -81,43 +91,126 @@ example (P : α → Prop) : ¬ (∃ x, P x) ↔ ∀ x, ¬ P x := by
 
 
 example {P Q : Prop} (h : P ∧ Q) : P ∨ Q := by
-  sorry
+  obtain ⟨hp, hq⟩ := h
+  left
+  apply hp
 
 example {P Q R : Prop} (h1 : P → Q) (h2 : P → R) (h3 : P) : Q ∧ R := by
-  sorry
+  constructor
+  apply h1
+  apply h3
+  apply h2
+  apply h3
 
 example (P : Prop) : ¬(P ∧ ¬ P) := by
-  sorry
+  intro h
+  obtain ⟨hp, hnotp⟩ := h
+  contradiction
 
 example {P Q : Prop} (h1 : P ↔ ¬ Q) (h2 : Q) : ¬ P := by
-  sorry
+  intro h
+  rw [h1] at h
+  contradiction
 
 example {P Q : Prop} (h1 : P ∨ Q) (h2 : Q → P) : P := by
-  sorry
+  obtain hp | hq := h1
+  apply hp
+  --rw [hq] at h2 -- only works for iff
+  apply h2
+  apply hq
 
 example {P Q R : Prop} (h : P ↔ Q) : (P ∧ R) ↔ (Q ∧ R) := by
-  sorry
+  constructor
+  · intro h2
+    obtain ⟨hp, hr⟩ := h2
+    rw [h] at hp
+    constructor
+    apply hp
+    apply hr
+  · intro h2
+    obtain ⟨hq, hr⟩ := h2
+    constructor
+    rw [h] -- accidental discovery that this is right?
+    apply hq
+    apply hr
 
 example (P : Prop) : (P ∧ P) ↔ P := by
-  sorry
+  constructor
+  · intro h
+    obtain ⟨hp, hp2⟩ := h
+    apply hp
+  · intro h
+    constructor <;> apply h
+
 
 example (P Q : Prop) : (P ∨ Q) ↔ (Q ∨ P) := by
-  sorry
+  constructor <;> intro h <;> obtain hl | hr := h
+  right
+  apply hl
+  left
+  apply hr
+  right
+  apply hl
+  left
+  apply hr
 
 example (P Q : Prop) : ¬(P ∨ Q) ↔ (¬P ∧ ¬Q) := by
-  sorry
+  constructor <;> intro h
+  · constructor
+    · intro hp
+      have : P ∨ Q := by
+        left
+        apply hp
+      contradiction
+    · intro hq
+      have : P ∨ Q := by
+        right
+        apply hq
+      contradiction
+  obtain ⟨hnp, hnq⟩ := h
+  intro h2
+  obtain hp | hq := h2
+  contradiction
+  contradiction
 
 example {P Q : α → Prop} (h1 : ∀ x, P x → Q x) (h2 : ∀ x, P x) : ∀ x, Q x := by
-  sorry
+  intro x
+  apply h1
+  apply h2
 
 example {P Q : α → Prop} (h : ∀ x, P x ↔ Q x) : (∃ x, P x) ↔ (∃ x, Q x) := by
-  sorry
+  constructor <;> intro h2 <;> obtain ⟨x, hx⟩ := h2
+  use x
+  rw [h] at hx
+  apply hx
+  use x
+  rw [h]
+  apply hx
 
 example (P : α → β → Prop) : (∃ x y, P x y) ↔ ∃ y x, P x y := by
-  sorry
+  constructor <;> intro h2 <;> obtain ⟨x, y, h3⟩ := h2
+  use y, x
+  apply h3
+  use y, x
+  apply h3
 
 example (P : α → β → Prop) : (∀ x y, P x y) ↔ ∀ y x, P x y := by
-  sorry
+  constructor <;> intro h
+  intro y x
+  apply h
+  intro x y
+  apply h
 
 example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
-  sorry
+  constructor <;> intro h
+  · obtain ⟨h1, hq⟩ := h
+    obtain ⟨x, h2⟩ := h1
+    use x
+    constructor
+    apply h2
+    apply hq
+  obtain ⟨x, hpx, hq⟩ := h
+  constructor
+  · use x
+    apply hpx
+  apply hq
